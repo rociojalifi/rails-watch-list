5793a15fe343df5b58fecac3dcbd7ee62c6cb33d
Rocio Jalifi <jalifirocio@gmail.com>
2021-06-24 23:28:57 +0200

    adds views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,41 @@
 class BookmarksController < ApplicationController
+before_action :set_bookmark, only: :destroy
+before_action :set_list, only: [:index, :new, :create]
+
+  def index
+    @bookmark = Bookmark.new
+  end
+  
+  def new
+	  @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    flash[:notice] = @bookmark.errors.full_messages.to_sentence unless @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def update
+    @bookmark = Bookmark.update(bookmark_params)
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-resources :lists  # resources :lists, except: [:edit, :update] do
-
-  # end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'lists#index'
+resources :lists, except: [:edit, :update] do
+  resources :bookmarks, only: [:create, :new]
+  resources :movies, only: :create
+  end
+  resources :bookmarks, only: [:destroy]
+  resources :movies, only: [:destroy]
 end
